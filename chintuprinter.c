234618b11ffdb6543d0438599fca56c133ef655b
Superty <aoe3tad@live.com>
2015-11-14 15:23:51 +0530

    Update chintuprinter.c
    Added command line arguments

--- chintuprinter.c
+++ chintuprinter.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("chintu!!!!!!!!");
+	if(argc == 1) printf("chintu!!!!!!!!");
+	else
+	{
+		int num = 0, i;
+		for(i = 0; argv[1][i] != '\0'; i++)
+		{
+			if(argv[1][i] < '0' || '9' < argv[1][i])
+			{
+				printf("Usage: %s [excitement-level]\n(if not specified, excitement-level = 8)\n", argv[0])
+				return 1;
+			}
+			num = 10*num + (argv[1][i] - '0');
+		}
+		printf("chintu");
+		for(i = 0; i < num; i++) printf("!");
+		printf("\n");
+	}
 	return 0;
 }
